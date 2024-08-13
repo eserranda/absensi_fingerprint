b43ffa39838ec3d0572ab862e13dd6436a098826
eserranda <eser.randa@gmail.com>
2024-08-14 00:17:20 +0800

    remove reset foem add jabsesni siswa

--- resources/views/data_absensi_siswa/data.blade.php
+++ resources/views/data_absensi_siswa/data.blade.php
@@ -603,7 +603,7 @@
                             });
                         } else {
                             console.log(data.message);
-                            form.reset();
+                            // form.reset();
                             $('#modal_add_data').modal('hide');
                             Swal.fire(
                                 'Tersimpan!',
